3915f58cac49ee3f4adba14dc95ae8c8d02de5bd
tanushamula <manikanta45-m>
2023-10-03 11:33:36 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using smtp_1.Models;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Mail;
+
+namespace smtp_1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult SendMail()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult SendMail(Email model)
+        {
+            try
+            {
+
+                MailMessage em = new MailMessage();
+                em.To.Add(model.To);
+                MailAddress addr = new MailAddress(model.Emails);
+                em.From = addr;
+                em.Subject = model.Subject;
+                em.Body = model.Body;
+
+                SmtpClient smtp = new SmtpClient();
+                smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+                smtp.EnableSsl = true;
+                smtp.Host = "smtp.gmail.com";
+                smtp.Port = 587;
+                NetworkCredential credential = new NetworkCredential(model.Emails, model.Password);
+                smtp.UseDefaultCredentials = true;
+                smtp.Credentials = credential;
+                smtp.Send(em);
+                ViewBag.Message = "Email sent";
+
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Message = ex.Message;
+            }
+            return View();
+
+          
+
+        }
+
+    }
+
+}
+    
---
+++ Models/Email.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+namespace smtp_1.Models
+{
+    public class Email
+    {
+        public string To { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+        public string Emails { get; set; }
+        public string Password { get; set; }    
+
+    }
+}
